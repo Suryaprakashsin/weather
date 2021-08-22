047d4a5931af15124eb88ec73ac7251614db1794
suryaprakash <singhyash2207@gmail.comm>
2021-08-22 11:26:53 +0530

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int degreesC = 15;
+        if (degreesC > 30){
+            System.out.println("It's hot outside!");
+        }else if(degreesC <5){
+            System.out.println("Brr, consider wearing jacket");
+        }else{
+            System.out.println("Not too hot, not too cold");
+        }
+    }
+}
